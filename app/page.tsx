fbd744e4cce68931f32c6a003fd76ba8c0451bf1
MattParki <mattparkinson_12@hotmail.com>
2025-09-15 21:05:07 +0100

    Comment out FuturePlansSection in Home component for future implementation. This change prepares the layout for upcoming features while maintaining current functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <HowItWorksSection />
       <ScreenshotsGallery />
       <FeaturesSection />
-      <FuturePlansSection />
+      {/* <FuturePlansSection /> */}
       <AboutSection />
       <FinalCTASection />
       
